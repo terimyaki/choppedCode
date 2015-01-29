e2ee6b873639cc9d68b620ac97a179214b592cf2
terimiyaki <wackoli123@yahoo.com>
2015-01-28 23:59:01 -0500

    was able to scrape the group types

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
+//Scraping www.javascripting.com for list of available Javascript frameworks
 var request = require("request");
 var cheerio = require("cheerio");
 
-request('http://www.javascripting.com', function(error, response, html){
+//
+var listOfGroups = [];
+
+request('https://www.javascripting.com', function(error, response, htmlText){
 	if (!error && response.statusCode == 200) {
-   		console.log(html);
-   		$ = cheerio.load(html);
+   		$ = cheerio.load(htmlText);
+
+   		//Get list of data items of all group types and links: ul id is "filter-sidebar"
+
+   		$("#filter-sidebar li").each(function(index){
+  			var self = $(this);
+   			if(self.data("item") !== "all" && self.hasClass("has_children") === false){
+   				var groupType = [];
+   				groupType.push(self.data("itemPretty"));
+   				groupType.push("https://www.javascripting.com" + self.find("a").attr("href"));
+   				listOfGroups.push(groupType);
+				}
+			});
 	}
+	console.log("i am here");
+	console.log(listOfGroups);
 });
+
+console.log(listOfGroups);
+   		//Go to each link in the list: ul id is "item-list"
+   		
+
+   		//Get last page number for page: class of div is "pagination bottom"
+   		//console.log($(".pagination bottom"));
+
+   		//Get list of data items on each page and its links, name, short description: ul id is "item-list"
+   		//console.log($("#item-list"));
 
 function createChoice(name, type, subtype, language, description, link){
 	//Create an object to be loaded into a database 
@@ -17,9 +44,5 @@ function createChoice(name, type, subtype, language, description, link){
 		language : language,
 		description : description,
 		link : link
-	}
-}
-
-function createLinksList(){
-
+	};
 }
